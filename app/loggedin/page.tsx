cf734882f2349ed2304592fca94bffb3ad3010ff
aditya-yadav08 <aditya.yadav2708@gmail.com>
2024-08-10 21:51:22 +0530

    Navbar

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,13 +1,18 @@
+import MobileNav from "@/components/nav/mobile-nav";
+import SideBar from "@/components/nav/side-bar";
 import Tasks from "@/components/todooo/tasks";
 import UserProfile from "@/components/todooo/user-profile";
 
-
 export default function Home() {
   return (
-    <main className="flex  flex-col items-center justify-between p-24">
-     <h1>Todooo</h1>
-     <UserProfile/>
-     <Tasks/>
-    </main>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <SideBar />
+      <div className="flex flex-col">
+        <MobileNav/>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+          <h1>Todooo</h1>
+        </main>
+      </div>
+    </div>
   );
 }
